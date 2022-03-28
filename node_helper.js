ee822e35d4ca1f7f790b78d712bdd4205933cfe9
Oliver Badike Hansen <oliverbadike@gmail.com>
2022-03-28 10:13:15 +0200

    Create node_helper.js
    Created nodehelper

---
+++ node_helper.js
@@ -0,0 +1,11 @@
+const NodeHelper = require("node_helper");
+module.exports = NodeHelper.create({});
+
+module.exports = NodeHelper.create({
+    
+    start: function () {
+        console.log('[WS281X-Server] Starting node_helper');
+        // Start API Endpoints
+        this.webServer()
+    },
+})
